bf225670f4e9debe266067bba187dd09e27cc943
Seif Ashraf <177072202+seif-ashraf-007@users.noreply.github.com>
2025-07-29 19:34:35 +0300

    fix: clerk auth middleware syntax error

--- server/server.js
+++ server/server.js
@@ -5,26 +5,21 @@ import connectDB from "./src/config/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import clerkWebhooks from "./src/middlewares/clerkWebhook.js";
 
+connectDB(env.MONGODB_URI);
+
 const app = express();
 app.use(cors());
-app.use(clerkMiddleware);
-app.use(express.json());
 
-// API to listen Clerk Webhook
-app.use("/api/clerk", clerkWebhooks);
+app.use(express.json());
+app.use(clerkMiddleware());
 
 app.get("/", (req, res) => {
   res.send("API is working");
 });
 
-(async () => {
-  try {
-    await connectDB(env.MONGODB_URI);
+// API to listen Clerk Webhook
+app.use("/api/clerk", clerkWebhooks);
 
-    app.listen(env.PORT, () => {
-      console.log(`✅ Server running: http://localhost:${env.PORT}`);
-    });
-  } catch (error) {
-    console.log(error, "Failed to start the server");
-  }
-})();
+app.listen(env.PORT, () => {
+  console.log(`✅ Server running: http://localhost:${env.PORT}`);
+});
